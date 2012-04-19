60d15fdb78032b8cc630476460c4f058957ab04b
Nicolas McCurdy <thenickperson@gmail.com>
2012-04-19 18:53:12 -0400

    semicolons, semicolons EVERYWHERE!

--- apps_dev/text_tools/scripts.js
+++ apps_dev/text_tools/scripts.js
@@ -1,75 +1,75 @@
 // ---VARIABLES---
-var effect = "normal" //change this to a different effect to bug test it on startup
-var focused, last_panel, content_array = []
-var char_count = "", word_count = "", line_count = "", find_count = ""
-var watched_input, watched_find, watched_replace, watched_list_start, watched_repetitions, watched_cutoff, working_input
-var all_panels = ["case_panel","find_panel","sort_panel","list_panel","other_panel","misc_panel","help_panel"]
-var all_elements = ["titlebar","toolbar","case_panel","find_panel","sort_panel","list_panel","other_panel","misc_panel","help_panel"]
-var hidden_elements = ["toolbar","case_panel","find_panel","sort_panel","list_panel","other_panel","misc_panel","help_panel"]
+var effect = "normal"; //change this to a different effect to bug test it on startup
+var focused, last_panel, content_array = [];
+var char_count = "", word_count = "", line_count = "", find_count = "";
+var watched_input, watched_find, watched_replace, watched_list_start, watched_repetitions, watched_cutoff, working_input;
+var all_panels = ["case_panel","find_panel","sort_panel","list_panel","other_panel","misc_panel","help_panel"];
+var all_elements = ["titlebar","toolbar","case_panel","find_panel","sort_panel","list_panel","other_panel","misc_panel","help_panel"];
+var hidden_elements = ["toolbar","case_panel","find_panel","sort_panel","list_panel","other_panel","misc_panel","help_panel"];
 
 $(function(){
 //initialization
 	$(".effects a:not(#help_panel)").click(function() {
-		clicked(this)
-	})
-	setInterval("watchInputs()",300)
-	setInterval("getHidden()",1000)
-	updateFocus($("#text_before"))
-	regainFocus()
-	clicked($("#normal"))
-	setInput()
-	panel("init")
+		clicked(this);
+	});
+	setInterval("watchInputs()",300);
+	setInterval("getHidden()",1000);
+	updateFocus($("#text_before"));
+	regainFocus();
+	clicked($("#normal"));
+	setInput();
+	panel("init");
 //start daemon scripts
-	$("body").keypress(watchInputs())
-	$("a").focus(regainFocus())
+	$("body").keypress(watchInputs());
+	$("a").focus(regainFocus());
 	$(".effect").click(function() {
-		clicked($(this))
-		toEffect($(this).attr("id"))
-	})
+		clicked($(this));
+		toEffect($(this).attr("id"));
+	});
 	$(".panel").mousedown(function() {
-		toggle($(".case_panel"))
-	})
-	$("#text_before").focus(updateFocus($('#text_before')))
-	$("#text_before").ready($('#text_before').focus())
-	$("#text_after").focus(updateFocus($('#text_after')))
+		toggle($(".case_panel"));
+	});
+	$("#text_before").focus(updateFocus($('#text_before')));
+	$("#text_before").ready($('#text_before').focus());
+	$("#text_after").focus(updateFocus($('#text_after')));
 	$("#text_after").click(function() {
-		selectAll($("#text_after"))
-	})
-	$("img:not([alt])").attr("alt","")
-})
+		selectAll($("#text_after"));
+	});
+	$("img:not([alt])").attr("alt","");
+});
 function getHidden() {
-	hidden_elements.length = 0
-	var counter = 0
+	hidden_elements.length = 0;
+	var counter = 0;
 	for(var i=0;i<all_elements.length;i++) {
-		var el = all_elements[i]
+		var el = all_elements[i];
 		if($("#"+el).is(":visible")==false) {
-			hidden_elements.push(el)
-			counter++
+			hidden_elements.push(el);
+			counter++;
 		}
 	}
-	setCookie("tt_hidden",hidden_elements.toString(),365)
+	setCookie("tt_hidden",hidden_elements.toString(),365);
 }
 
 //collapse and expand all sections
 function panel() {
-	var panels = Array.prototype.slice.call(arguments)
+	var panels = Array.prototype.slice.call(arguments);
 	if(panels[0]=="init") {
 		if(getCookie("tt_hidden")) {
-			hidden_elements = getCookie("tt_hidden").split(",")
+			hidden_elements = getCookie("tt_hidden").split(",");
 		}
 		for(var i=0;i<hidden_elements.length;i++) {
-			$("#"+hidden_elements[i]).hide()
+			$("#"+hidden_elements[i]).hide();
 		}
 	}
 	else {
-		var displayMode = (panels[0] == "collapse" ? "none" : "")
+		var displayMode = (panels[0] == "collapse" ? "none" : "");
 		if(panels[1]=="all") {
 			for(var i=0;i<all_panels.length;i++) {
 				if(panels[0]=="expand") {
-					$("#"+all_panels[i]).show()
+					$("#"+all_panels[i]).show();
 				}
 				if(panels[0]=="collapse") {
-					$("#"+all_panels[i]).hide()
+					$("#"+all_panels[i]).hide();
 				}
 			}
 		}
@@ -77,14 +77,14 @@ function panel() {
 		{
 			for(var i=1;i<panels.length;i++) {
 				if(panels[0]=="expand") {
-					$("#"+panels[i]).show()
+					$("#"+panels[i]).show();
 				}
 				if(panels[0]=="collapse") {
-					$("#"+panels[i]).hide()
+					$("#"+panels[i]).hide();
 				}
 			}
 		}
-		getHidden()
+		getHidden();
 	}
 }
 
@@ -98,7 +98,7 @@ function getUrlVars() {
 }
 function setInput() {
 	if(decodeURIComponent(getUrlVars()["before"])!="undefined") {
-		$("#text_before").val() = decodeURIComponent(getUrlVars()["before"])
+		$("#text_before").val() = decodeURIComponent(getUrlVars()["before"]);
 	}
 }
 
@@ -107,7 +107,7 @@ function setInput() {
 // ---CONVERSION FUNCTIONS---
 
 function convert() {
-	watched_input = $("#text_before").val()
+	watched_input = $("#text_before").val();
 	
 	if($("find_text").val()!="") {
 		if($("#regexp_toggle").attr("checked", false)) {
@@ -123,106 +123,106 @@ function convert() {
 	
 	switch(effect) {
 		case "normal":
-			update(watched_input)
-			break
+			update(watched_input);
+			break;
 		case "lowercase":
-			update(watched_input.toLowerCase())
-			break
+			update(watched_input.toLowerCase());
+			break;
 		case "uppercase":
-			update(watched_input.toUpperCase())
-			break
+			update(watched_input.toUpperCase());
+			break;
 		case "htmllower":
-			htmlCase("lower")
-			break
+			htmlCase("lower");
+			break;
 		case "htmlupper":
-			htmlCase("upper")
-			break
+			htmlCase("upper");
+			break;
 		case "titlecase":
-			update(watched_input.toLowerCase().toTitleCase())
-			break
+			update(watched_input.toLowerCase().toTitleCase());
+			break;
 		case "find":
 			if($("#regexp_toggle").attr("checked", false)) {
-				$("#text_after").val(watched_input.replace($("#find_test").val(), $("#find_text").val().toUpperCase()))
+				$("#text_after").val(watched_input.replace($("#find_test").val(), $("#find_text").val().toUpperCase()));
 			}
 			else {
-				$("#text_after").val(watched_input.replace(new RegExp($("#find_text").val(), "gi"), ("#find_text").val().toUpperCase()))
+				$("#text_after").val(watched_input.replace(new RegExp($("#find_text").val(), "gi"), ("#find_text").val().toUpperCase()));
 			}
-			break
+			break;
 		case "replace":
 			if($("#regexp_toggle").attr("checked", false)) {
-				$("#text_after").val(watched_input.replace($("#find_text").val(), $("#replace_text").val()))
+				$("#text_after").val(watched_input.replace($("#find_text").val(), $("#replace_text").val()));
 			}
 			else {
-				$("#text_after").val(watched_input.replace(new RegExp($("#find_text").val(), "gi"), $("#replace_text").val()))
+				$("#text_after").val(watched_input.replace(new RegExp($("#find_text").val(), "gi"), $("#replace_text").val()));
 			}
-			break
+			break;
 		case "list":
 			if($("#number_list").attr("checked")) {
-				listNumbers()
+				listNumbers();
 			}
 			else {
-				$("#text_after").val($("#list_start").val()+watched_input.replace(new RegExp("\n","gi"),"\n"+$("#list_start").val()))
+				$("#text_after").val($("#list_start").val()+watched_input.replace(new RegExp("\n","gi"),"\n"+$("#list_start").val()));
 			}
-			break
+			break;
 		case "remove_list":
-			text_array = watched_input.split("\n")
+			text_array = watched_input.split("\n");
 			for (i=0;i<text_array.length;i++) {
-				text_array[i] = text_array[i].substring(watched_cutoff)
+				text_array[i] = text_array[i].substring(watched_cutoff);
 			}
-			update(text_array.join("\n"))
-			break
+			update(text_array.join("\n"));
+			break;
 		case "repeat":
-			update(watched_input.repeat(parseInt($("#repetitions").text())))
-			break
+			update(watched_input.repeat(parseInt($("#repetitions").text())));
+			break;
 		case "wordcount":
-			char_count = watched_input.length
-			line_count = watched_input.split("\n")
-			line_count = line_count.length
-			word_count = watched_input.split(" ")
-			word_count = word_count.length + line_count - 1
+			char_count = watched_input.length;
+			line_count = watched_input.split("\n");
+			line_count = line_count.length;
+			word_count = watched_input.split(" ");
+			word_count = word_count.length + line_count - 1;
 			if(watched_input=="") {
-				word_count=0
+				word_count=0;
 			}
-			update("Characters: "+char_count+"\nWords: "+word_count+"\nLines: "+line_count)
-			break
+			update("Characters: "+char_count+"\nWords: "+word_count+"\nLines: "+line_count);
+			break;
 		case "sortaz":
-			text_array = watched_input.split("\n")
-			text_array = text_array.sort()
-			update(text_array.join("\n"))
-			break
+			text_array = watched_input.split("\n");
+			text_array = text_array.sort();
+			update(text_array.join("\n"));
+			break;
 		case "sortza":
-			text_array = watched_input.split("\n")
-			text_array = text_array.sort()
-			text_array = text_array.reverse()
-			update(text_array.join("\n"))
-			break
+			text_array = watched_input.split("\n");
+			text_array = text_array.sort();
+			text_array = text_array.reverse();
+			update(text_array.join("\n"));
+			break;
 		case "sortreverse":
-			text_array = watched_input.split("\n")
-			text_array = text_array.reverse()
-			update(text_array.join("\n"))
-			break
+			text_array = watched_input.split("\n");
+			text_array = text_array.reverse();
+			update(text_array.join("\n"));
+			break;
 		case "sortrandom":
-			text_array = watched_input.split("\n")
-			text_array = text_array.sort(function() {return 0.5 - Math.random()})
-			update(text_array.join("\n"))
-			break
+			text_array = watched_input.split("\n");
+			text_array = text_array.sort(function() {return 0.5 - Math.random()});
+			update(text_array.join("\n"));
+			break;
 		case "rot13":
 			update(watched_input.replace(/[a-zA-Z]/g, function(c){
-				return String.fromCharCode((c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26)
-			}))
-			break
+				return String.fromCharCode((c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26);
+			}));
+			break;
 		case "backwards":
-			var backwards = ""
+			var backwards = "";
 			for (i = 0; i <= watched_input.length; i++) {
-				backwards = watched_input.substring(i, i+1) + backwards
+				backwards = watched_input.substring(i, i+1) + backwards;
 			}
-			update(backwards)
-			break
+			update(backwards);
+			break;
 	}
 	
 	//output autoselect exclusion
 	if(focused==$("#text_after") && effect!="find" && effect!="replace" && effect!="list" && effect!="remove_list" && effect!="repeat") {
-		selectAll($("#text_after"))
+		selectAll($("#text_after"));
 	}
 }
 
@@ -238,17 +238,17 @@ function watchInputs() { //input watcher
 		convert();
 		/*console.log("main watcher activated");*/
 	}
-	watcher(watched_find,$("#find_text"),"find") //buggy
-	watcher(watched_replace,$("#replace_text"),"replace") //buggy
-	watcher(watched_list_start,$("#list_start"),"list_start") //buggy
-	watcher(watched_repetitions,$("#repetitions"),"repetitions") //buggy
-	watcher(watched_cutoff,$("#cutoff"),"cutoff") //buggy
+	watcher(watched_find,$("#find_text"),"find"); //buggy
+	watcher(watched_replace,$("#replace_text"),"replace"); //buggy
+	watcher(watched_list_start,$("#list_start"),"list_start"); //buggy
+	watcher(watched_repetitions,$("#repetitions"),"repetitions"); //buggy
+	watcher(watched_cutoff,$("#cutoff"),"cutoff"); //buggy
 }
 function outputToInput() {
-	$("#text_before").val($("#text_after").val())
+	$("#text_before").val($("#text_after").val());
 }
 function update(change) {
-	$("#text_after").val(change)
+	$("#text_after").val(change);
 }
 function toEffect(change) {
 	effect = change;
@@ -259,11 +259,11 @@ function selectAll(field) {
 	field.select();
 }
 function clear() {
-	$("#text_before,#find_text,#replace_text").val("")
-	$("#list_start").val("- ")
-	$("#cutoff").val("3")
-	$("#repetitions").val("1")
-	$("#number_list").attr("checked", false)
+	$("#text_before,#find_text,#replace_text").val("");
+	$("#list_start").val("- ");
+	$("#cutoff").val("3");
+	$("#repetitions").val("1");
+	$("#number_list").attr("checked", false);
 }
 function updateFocus(newFocus) {
 	focused = newFocus;
@@ -273,10 +273,10 @@ function regainFocus() {
 }
 function toggleCheck(element) {
 	if(element.checked==true) {
-		element.checked=false
+		element.checked=false;
 	}
 	else {
-		element.checked=true
+		element.checked=true;
 	}
 }
 
@@ -284,18 +284,18 @@ function toggleCheck(element) {
 
 //plus and minus buttons
 function valueUp(variable) {
-	variable.value=parseInt(variable.value)+1
+	variable.value=parseInt(variable.value)+1;
 }
 function valueDown(variable) {
-	variable.value=parseInt(variable.value)-1
+	variable.value=parseInt(variable.value)-1;
 	if(parseInt(variable.value)<0){
-		variable.value="0"
+		variable.value="0";
 	}
 }
 
 //numbers only
 function numbersOnly(obj) {
-	obj.value = obj.value.replace(/\D/, '')
+	obj.value = obj.value.replace(/\D/, '');
 }
 
 //selected button stuff
@@ -316,37 +316,37 @@ function clicked(clickedOn) {
 
 //awesome html case changer by me :D
 function htmlCase(html_case_mode) {
-	working_input = watched_input
-	content_array = []
+	working_input = watched_input;
+	content_array = [];
 	for (i=1;i<watched_input.split(new RegExp("<", "gi")).length;i++) { //set the input into an array
 		if(working_input.indexOf("<")!=-1) {
-			content_array.push(working_input.substring(0,working_input.indexOf("<"))+"<")
-			working_input = working_input.substring(working_input.indexOf("<")+1)
+			content_array.push(working_input.substring(0,working_input.indexOf("<"))+"<");
+			working_input = working_input.substring(working_input.indexOf("<")+1);
 		}
 		if(working_input.indexOf(">")!=-1) {
-			content_array.push(working_input.substring(0,working_input.indexOf(">"))+">")
-			working_input = working_input.substring(working_input.indexOf(">")+1)
+			content_array.push(working_input.substring(0,working_input.indexOf(">"))+">");
+			working_input = working_input.substring(working_input.indexOf(">")+1);
 		}
 	}
-	content_array.push(working_input)
+	content_array.push(working_input);
 	for (i=1;i<content_array.length;i=i+2) { //change the case of html
-		if(html_case_mode=="lower")content_array[i] = content_array[i].toLowerCase()
-		if(html_case_mode=="upper")content_array[i] = content_array[i].toUpperCase()
+		if(html_case_mode=="lower")content_array[i] = content_array[i].toLowerCase();
+		if(html_case_mode=="upper")content_array[i] = content_array[i].toUpperCase();
 	}
-	update(content_array.join("")) //set this value as blank when not bug testing
+	update(content_array.join("")); //set this value as blank when not bug testing
 }
 
 //numbered lists
 function listNumbers() {
-	working_input = ""
-	content_array = []
-	content_array = watched_input.split(new RegExp("\n", "gi"))
-	var list_length = content_array.length
-	var num = 0
+	working_input = "";
+	content_array = [];
+	content_array = watched_input.split(new RegExp("\n", "gi"));
+	var list_length = content_array.length;
+	var num = 0;
 	for (i=0;i<list_length;i++) { //act sequentially on all array items
-		working_input = working_input + ++num + $("#list_start").val() + content_array.shift() + "\n"
+		working_input = working_input + ++num + $("#list_start").val() + content_array.shift() + "\n";
 	}
-	update(working_input.slice(0,working_input.length-1))
+	update(working_input.slice(0,working_input.length-1));
 }
 
 /* 
