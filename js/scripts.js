8a050c0469bcbd80688565a7ad3a3d32a3368722
Nicolas McCurdy <thenickperson@gmail.com>
2012-04-24 15:49:22 -0400

    add smart email links and some other random fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
 	$('#projects-nav').scrollspy();
 });
+
+if($(".email").length){
+	// variables, which will be replaced
+	var at = / AT /;
+	var dot = / DOT /g;
+
+	// function, which replaces pre-made class
+	$(".email a").each(function () {
+		var address = "mailto:" + $(this).data("email").replace(at, '@').replace(dot, '.');
+		$(this).attr("href",address);
+	});
+	$(".email").show();
+};
