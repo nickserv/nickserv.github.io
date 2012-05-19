8eb9e210d2bac5425917e02244468c2b4cc5b989
Nicolas McCurdy <thenickperson@gmail.com>
2012-05-19 19:57:34 -0400

    lim: add basic new message functionality and tweak display of messages

--- apps_dev/lim/scripts.js
+++ apps_dev/lim/scripts.js
@@ -1,5 +1,27 @@
-/*
-$(window).resize(function() {
-	$("chat").height( $(document).height() - $("#toolbar").height() - $("input").height() );
+$(document).bind("keyup keydown", function(e) {
+	shifted = e.shiftKey;
 });
-*/
+
+function newMessage() {
+	var message = $("#input-box").val();
+	message = message.replace(/\n/g, "<br>");
+	$("#chat").append("<div>"+message+"</div>");
+	$("#input-box").val("");
+}
+
+//when something is typed in the input box
+$("#input-box").keyup(function(e) {
+	//if it was return (without shift)
+	if(e.keyCode == 13 && !shifted) {
+		//if there is a message in the input box
+		if($("#input-box").val != "") {
+			//submit a new IM
+			newMessage();
+		}
+		//if the input box is empty
+		else {
+			//switch active user
+			switchUser();
+		}
+	};
+});
