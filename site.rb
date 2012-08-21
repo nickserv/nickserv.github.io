37211b77c0d803c2e29abc992940fab1ec003edb
Nicolas McCurdy <thenickperson@gmail.com>
2012-08-21 01:24:51 -0400

    Add basic site.rb file for pointing to different URLs (using sinatra and erb)

---
+++ site.rb
@@ -0,0 +1,14 @@
+require 'rubygems'
+require 'sinatra'
+
+get '/index.html' do
+	erb :'index.html'
+end
+
+get '/projects.html' do
+	erb :'projects.html'
+end
+
+get '/skills.html' do
+	erb :'skills.html'
+end
