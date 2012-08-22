6f166c71cc67ea9fcb48ead1ddee9d280058284e
Nicolas McCurdy <thenickperson@gmail.com>
2012-08-22 15:54:57 -0400

    Rename pages and routes in site.rb to point to files ending in .erb

--- site.rb
+++ site.rb
@@ -2,30 +2,28 @@ require 'sinatra/base'
 
 class MySite < Sinatra::Base
 
-	Tilt.register Tilt::ERBTemplate, 'html.erb'
-
-	get '/index.html' do
+	get '/index' do
 		@page_title = 'Home'
-		@page_id = 'index.html'
-		erb :'index.html'
+		@page_id = 'index'
+		erb :'index'
 	end
 
-	get '/projects.html' do
+	get '/projects' do
 		@page_title = 'Projects'
-		@page_id = 'projects.html'
-		erb :'projects.html'
+		@page_id = 'projects'
+		erb :'projects'
 	end
 
-	get '/skills.html' do
+	get '/skills' do
 		@page_title = 'Skills'
-		@page_id = 'skills.html'
-		erb :'skills.html'
+		@page_id = 'skills'
+		erb :'skills'
 	end
 
 	not_found do
 		@page_title = '404 Not Found'
-		@page_id = '404.html'
-		erb :'404.html'
+		@page_id = '404'
+		erb :'404'
 	end
 
 end
