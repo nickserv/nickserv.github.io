00f1ee17b8bddb84cc54398df79f227c3f2a81d6
Nicolas McCurdy <thenickperson@gmail.com>
2012-08-22 02:05:40 -0400

    Move common HTML content (start and end of pages) into layout.html.erb

--- site.rb
+++ site.rb
@@ -2,17 +2,25 @@ require 'rubygems'
 require 'sinatra'
 
 get '/index.html' do
-	erb :'index.html'
+	@page_title = 'Home'
+	@page_id = 'index.html'
+	erb :'index.html', { :layout => :'layout.html' }
 end
 
 get '/projects.html' do
-	erb :'projects.html'
+	@page_title = 'Projects'
+	@page_id = 'projects.html'
+	erb :'projects.html', { :layout => :'layout.html' }
 end
 
 get '/skills.html' do
-	erb :'skills.html'
+	@page_title = 'Skills'
+	@page_id = 'skills.html'
+	erb :'skills.html', { :layout => :'layout.html' }
 end
 
 not_found do
-	erb :'404.html'
+	@page_title = '404 Not Found'
+	@page_id = '404.html'
+	erb :'404.html', { :layout => :'layout.html' }
 end
