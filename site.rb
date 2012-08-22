1ea4e3fc28215755e2f29bc5249dc5865020dc10
Nicolas McCurdy <thenickperson@gmail.com>
2012-08-22 10:44:56 -0400

    Register .html.erb as the default extension for views

--- site.rb
+++ site.rb
@@ -1,26 +1,28 @@
 require 'rubygems'
 require 'sinatra'
 
+Tilt.register Tilt::ERBTemplate, 'html.erb'
+
 get '/index.html' do
 	@page_title = 'Home'
 	@page_id = 'index.html'
-	erb :'index.html', { :layout => :'layout.html' }
+	erb :'index.html'
 end
 
 get '/projects.html' do
 	@page_title = 'Projects'
 	@page_id = 'projects.html'
-	erb :'projects.html', { :layout => :'layout.html' }
+	erb :'projects.html'
 end
 
 get '/skills.html' do
 	@page_title = 'Skills'
 	@page_id = 'skills.html'
-	erb :'skills.html', { :layout => :'layout.html' }
+	erb :'skills.html'
 end
 
 not_found do
 	@page_title = '404 Not Found'
 	@page_id = '404.html'
-	erb :'404.html', { :layout => :'layout.html' }
+	erb :'404.html'
 end
