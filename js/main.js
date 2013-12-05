60818bed5633324b6effd53e05fb76cc956d4796
Nicolas McCurdy <thenickperson@gmail.com>
2013-12-05 05:37:01 -0500

    Wrap custom JavaScript in a jQuery document ready callback

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
-if($(".email").length){
-  // variables, which will be replaced
-  var at = / AT /;
-  var dot = / DOT /g;
+$(document).ready(function () {
+  if($(".email").length){
+    // variables, which will be replaced
+    var at = / AT /;
+    var dot = / DOT /g;
 
-  // function, which replaces pre-made class
-  $(".email a").each(function () {
-    var address = "mailto:" + $(this).data("email").replace(at, "@").replace(dot, ".");
-    $(this).attr("href",address);
-  });
-  $(".email").show();
-};
+    // function, which replaces pre-made class
+    $(".email a").each(function () {
+      var address = "mailto:" + $(this).data("email").replace(at, "@").replace(dot, ".");
+      $(this).attr("href",address);
+    });
+    $(".email").show();
+  };
+});
