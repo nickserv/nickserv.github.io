45c320d1c5ed7dbe89e6be28774fa9bd19b9cf07
Nicolas McCurdy <thenickperson@gmail.com>
2013-12-05 14:30:49 -0500

    Fix jslint issues

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ lint: jslint csslint
 
 # Run jslint on all JavaScript code
 jslint:
-	jslint js/main.js --terse --browser --indent=2 --plusplus --predef $$
+	jslint js/main.js --terse --browser --indent=2 --predef $$
 
 # Run csslint on all CSS code
 csslint:
--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
+"use strict";
+
 $(document).ready(function () {
-  if($(".email").length){
+  if ($(".email").length) {
     // variables, which will be replaced
-    var at = / AT /;
-    var dot = / DOT /g;
+    var at = / AT /,
+      dot = / DOT /g;
 
     // function, which replaces pre-made class
     $(".email a").each(function () {
       var address = "mailto:" + $(this).data("email").replace(at, "@").replace(dot, ".");
-      $(this).attr("href",address);
+      $(this).attr("href", address);
     });
     $(".email").show();
-  };
+  }
 });
