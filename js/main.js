f5db06453e78a58724c532dd02c9e29184b6ac7d
Nicolas McCurdy <thenickperson@gmail.com>
2014-04-23 03:22:13 -0400

    Remove the email address decoding script so we can use the email link normally

--- js/main.js
+++ js/main.js
@@ -1,25 +1,7 @@
 "use strict";
 
-function decodeEmail () {
-  // Email link decoding
-  if ($(".email").length) {
-    // variables, which will be replaced
-    var at = / AT /,
-      dot = / DOT /g;
-
-    // function, which replaces pre-made class
-    $(".email").each(function () {
-      var address = "mailto:" + $(this).data("email").replace(at, "@").replace(dot, ".");
-      $(this).attr("href", address);
-    });
-    $(".email").css("display", "inherit");
-  }
-}
-
 $(document).ready(function () {
   // Smooth scroll
   // See https://github.com/kswedberg/jquery-smooth-scroll
   $("a").smoothScroll();
-
-  decodeEmail();
 });
