80db33d606d4219b08c574ab2fa716d0495137d8
Nick McCurdy <nick@nickmccurdy.com>
2018-02-14 07:50:46 -0500

    Authenticate octokit with a token

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,5 @@
 const crypto = require('crypto')
+const dotenv = require('dotenv')
 const fs = require('fs')
 const { flatMap } = require('lodash')
 const rest = require('@octokit/rest')
@@ -8,12 +9,16 @@ const { safeLoad } = require('js-yaml')
 const octokit = rest()
 const readFile = promisify(fs.readFile)
 
+dotenv.config()
+
 exports.sourceNodes = async ({ boundActionCreators: { createNode } }) => {
   const projects = await readFile('_data/projects.yml', 'utf8')
 
   const repos = flatMap(safeLoad(projects), category =>
     category.filter(project => project.github).map(project => project.github)
   )
+
+  octokit.authenticate({ type: 'token', token: process.env.GITHUB_TOKEN })
 
   return Promise.all(
     repos.map(async repo => {
